3e481f2e82b03ae7403b42fb67e0691f7c88fa65
whiteyeOwO <betty06231227@gmail.com>
2024-11-20 10:43:57 +0800

    1120

--- my- react-todo/todo-demo/src/Todo.jsx
+++ my- react-todo/todo-demo/src/Todo.jsx
@@ -1,17 +1,19 @@
 import { MdDelete, MdEdit } from "react-icons/md";
+import EditForm from "./editForm";
 
-function Todo({todo, deleteTodo , toggleCompleted}){
-    return(   
-        <div className={`todo ${todo.isCompleted ? "completed": ""}`}>
-            <p onClick={() => {toggleCompleted(todo.id)}}>{todo.content}</p>
-            <div>
-                <MdEdit style={{cursor:"pointer"}}/>
-                <MdDelete onClick={() => {
-                    deleteTodo(todo.id)
-                }} 
-                style={{cursor:"pointer",marginLeft:"5px"}}/>
+function Todo({ todo, deleteTodo, toggleCompleted, toggleIsEdit, editTodo }) {
+    return (
+        todo.isEdit
+            ? <EditForm todo={todo} editTodo = {editTodo} />
+            : <div className={`todo ${todo.isCompleted ? "completed" : ""}`}>
+                <p onClick={() => { toggleCompleted(todo.id) }}>{todo.content}</p>
+                <div>
+                    <MdEdit onClick={() => { toggleIsEdit(todo.id) }} style={{ cursor: "pointer" }} />
+                    <MdDelete onClick={() => {
+                        deleteTodo(todo.id)
+                    }} style={{ cursor: "pointer", marginLeft: "5px" }} />
+                </div>
             </div>
-        </div>
     )
 }
 
--- my- react-todo/todo-demo/src/TodoWrapper.jsx
+++ my- react-todo/todo-demo/src/TodoWrapper.jsx
@@ -3,13 +3,14 @@ import CreateForm from "./CreateForm";
 import Todo from "./Todo";
 
 function TodoWrapper() {
+
     const [todos, setTodos] = useState([
-        { content: "L1", id: 1 ,isCompleted:false},
-        { content: "L2", id: 2 ,isCompleted:false},
+        { content: "L1", id: 1, isCompleted:false, isEdit:false},
+        { content: "L2", id: 2, isCompleted:false, isEdit:false},
     ]);
 
     const addTodo = (content) => {
-        setTodos([...todos, { content: content, id: Math.random(),isCompleted:false}]);
+        setTodos([...todos, { content: content, id: Math.random(),isCompleted:false,isEdit:false}]);
     };
 
     const deleteTodo = (id) => {
@@ -26,13 +27,29 @@ function TodoWrapper() {
         }))
     }
 
+    const toggleIsEdit = (id) => {
+        setTodos(todos.map((todo) => {
+            return todo.id === id
+            ? {...todo, isEdit: !todo.isEdit}
+            : todo
+        }))
+    }
+
+    const editTodo = (id, newContent) => {
+        setTodos(todos.map((todo) => {
+            return todo.id === id
+            ? {...todo, content: newContent, isEdit: false}
+            : todo
+        }))
+    }
+
     return (
         <div className="wrapper">
             <h1>待辦事項</h1>
             <CreateForm addTodo={addTodo}/>
             {todos.map((todo) => {
                 return <Todo  todo={todo} key={todo.id} 
-                    deleteTodo = {deleteTodo} toggleCompleted = {toggleCompleted}/>;
+                    deleteTodo = {deleteTodo} toggleCompleted = {toggleCompleted} toggleIsEdit = {toggleIsEdit} editTodo={editTodo}/>;
             })}
         </div>
     );
