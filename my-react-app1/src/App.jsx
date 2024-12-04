07920ac4be86854963dc80b2ce884a257c70face
whiteyeOwO <funlabfunlab@gmail.com>
2024-12-04 11:44:54 +0800

    1204

--- my-react-app1/src/App.jsx
+++ my-react-app1/src/App.jsx
@@ -1,75 +1,64 @@
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css"
 import { IoUmbrellaSharp } from "react-icons/io5";
+import axios from 'axios';
 
 function App() {
+  const [weatherList, setWeatherList] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const data = await axios.get("https://whiteyeowo.github.io/ReactEX/json/F-C0032-001.json");
+
+      const { location } = data.data.cwaopendata.dataset;
+      setWeatherList(location);
+
+      // locationName => 縣市名
+      // elementName => Wx => 天氣概況
+      // elementName => PoP => 降雨率
+    })()
+  }, [])
 
 
   return (
     <>
+
       <div className="wrap">
         <h2>36小時天氣預報</h2>
-        <div className="row">
+        <div className="container">
 
           {
-            location.map((city) => {
+            weatherList.map((city) => {
               return (
-                <div className="col">
-                  <div className="card">
-                    <div className="card-title">
-                      {city.locationName}
-                    </div>
-                    <div className="card-body">
-                      <div className="row2">
+                <div className="item" key={city.locationName}>
 
-                        <div className="col2">
-                          <p>2日</p>
-                          <p>
-                            上午6點<br />
-                            ~<br />
-                            下午6點
-                          </p>
-                          <p>
-                            <img src="./public/images/weatherIcon/晴時多雲.svg" alt="" />
-                          </p>
-                          <p>晴時多雲</p>
-                          <p><IoUmbrellaSharp />10%</p>
-                        </div>
-
-                        <div className="col2">
-                          <p>3日</p>
-                          <p>
-                            上午6點<br />
-                            ~<br />
-                            下午6點
-                          </p>
-                          <p>
-                            <img src="./public/images/weatherIcon/晴時多雲.svg" alt="" />
-                          </p>
-                          <p>晴時多雲</p>
-                          <p><IoUmbrellaSharp />10%</p>
-                        </div>
-
-                        <div className="col2">
-                          <p>4日</p>
-                          <p>
-                            上午6點<br />
-                            ~<br />
-                            下午6點
-                          </p>
-                          <p>
-                            <img src="./public/images/weatherIcon/晴時多雲.svg" alt="" />
-                          </p>
-                          <p>晴時多雲</p>
-                          <p><IoUmbrellaSharp />10%</p>
-                        </div>
-
-                      </div>
-                    </div>
+                  <div className="title">
+                    <h3>{city.locationName}</h3>
                   </div>
-
+                  <div className="content">
+                    {
+                      city.weatherElement[0].time.map((time, index) => {
+                        return(
+                          <div className="item2" key={index}>
+                      <p>{new Date(time.startTime).toLocaleString(undefined,{day: "numeric"})}</p>
+                      <p>
+                      {new Date(time.startTime).toLocaleString(undefined,{hour: "numeric", minute:"numeric"})}<br />
+                        ~<br />
+                        {new Date(time.endTime).toLocaleString(undefined,{hour: "numeric", minute:"numeric"})}
+                      </p>
+                      <p>
+                        <img src={`./public/images/weatherIcon/${time.parameter.parameterName}.svg`} alt="" />
+                      </p>
+                      <p>{time.parameter.parameterName}</p>
+                      <p><IoUmbrellaSharp />{city.weatherElement[4].time[index].parameter.parameterName}%</p>
+                    </div>
+                        )
+                      })
+    
+                    }
+                  </div>
                 </div>
+
               )
             })
           }
